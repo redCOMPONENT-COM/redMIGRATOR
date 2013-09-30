57c2cd30d2271f8bcc1567b32666e23d9bc32b71
vdkhai <khai@redweb.dk>
2013-09-30 15:29:43 +0700

    Add virtuemart plugin (3rd party)

---
+++ plugins/3rd_party/plg_jupgradepro_virtuemart/jupgradepro_virtuemart.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * jUpgrade Virtuemart Component adapter
+ *
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @author		Matias Aguirre <maguirre@matware.com.ar>
+ * @copyright	Copyright (C) 2004 - 2013 Matware. All rights reserved.
+ * @license		GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ * @link		http://www.matware.com.ar
+ */
+defined('_JEXEC') or die;
+
+/**
+ * jUpgradePro Virtuemart Plugin
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  system.jupgradepro
+ * @since       3.0.1
+ */
+class plgJUpgradeProVirtuemart extends JPlugin
+{
+
+}
