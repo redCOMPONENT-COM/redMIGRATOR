d5603b3c63d6e48130268e32401c3452fcc7dde2
vdkhai <khai@redweb.dk>
2013-10-25 17:08:39 +0700

    Migrate K2 Component

---
+++ plugins/3rd_party/plg_redmigrator_k2/sql/uninstall.utf8.sql
@@ -0,0 +1,12 @@
+DROP TABLE
+`#__k2_attachments`,
+`#__k2_categories`,
+`#__k2_comments`,
+`#__k2_extra_fields`,
+`#__k2_extra_fields_groups`,
+`#__k2_items`,
+`#__k2_rating`,
+`#__k2_tags`,
+`#__k2_tags_xref`,
+`#__k2_users`,
+`#__k2_user_groups`;
