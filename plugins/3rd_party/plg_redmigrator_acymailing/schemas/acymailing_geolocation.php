28a40c2fba439e0609d0d8013ba98a70ed3020cf
vdkhai <khai@redweb.dk>
2013-10-30 15:01:03 +0700

    Migrate component Acymailing

---
+++ plugins/3rd_party/plg_redmigrator_acymailing/schemas/acymailing_geolocation.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * 
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+
+class RedMigratorAcymailingGeolocation extends RedMigrator
+{
+}
+?>
