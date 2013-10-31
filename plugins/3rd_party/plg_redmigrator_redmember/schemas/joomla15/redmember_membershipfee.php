9af13c8dff1d7e9f68131ab60210cf5050670f78
vdkhai <khai@redweb.dk>
2013-10-31 15:27:19 +0700

    Migrate membership, user, userytype (redMEMBER)

---
+++ plugins/3rd_party/plg_redmigrator_redmember/schemas/joomla15/redmember_membershipfee.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * 
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+
+class RedMigratorRedmemberMembershipfee extends RedMigrator
+{
+    public function dataHook($rows)
+    {
+        $arrFields = array('membership_id',
+                            'membership_name',
+                            'user_group_ids',
+                            'period',
+                            'price',
+                            'published'
+                        );
+
+        // Do some custom post processing on the list.
+        foreach ($rows as &$row)
+        {
+            $row = (array) $row;
+
+            // Change fields' name
+            if (isset($row['membershipfee_id']))
+            {
+                $row['membership_id'] = $row['membershipfee_id'];    
+            }
+
+            if (isset($row['membershipfee_name']))
+            {
+                $row['membership_name'] = $row['membershipfee_name'];
+            }
+
+            if (isset($row['user_type']))
+            {
+                $row['user_group_ids'] = $row['user_type'];    
+            }
+
+            if (isset($row['membershipfee_period']))
+            {
+                $row['period'] = $row['membershipfee_period'];    
+            }
+
+            if (isset($row['membershipfee_price']))
+            {
+                $row['price'] = $row['membershipfee_price'];    
+            }
+
+            foreach ($row as $key => $value)
+            {
+                if (!in_array($key, $arrFields))
+                {
+                    unset($row[$key]);
+                }
+            }
+        }
+
+        return $rows;
+    }
+}
+?>
---
+++ plugins/3rd_party/plg_redmigrator_redmember/schemas/joomla15/redmember_usertype.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * 
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+
+class RedMigratorRedmemberUserType extends RedMigrator
+{
+    public function dataHook($rows)
+    {
+        $arrFields = array('usergroup_id',
+                            'payment_required',
+                            'tax_exempt'
+                        );
+
+        // Do some custom post processing on the list.
+        foreach ($rows as &$row)
+        {
+            $row = (array) $row;
+
+            // Change fields' name
+            if (isset($row['usertype_id']))
+            {
+                $row['usergroup_id'] = $row['usertype_id'];    
+            }
+
+            foreach ($row as $key => $value)
+            {
+                if (!in_array($key, $arrFields))
+                {
+                    unset($row[$key]);
+                }
+            }
+        }
+
+        return $rows;
+    }
+}
+?>
