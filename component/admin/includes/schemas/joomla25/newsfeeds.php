0d26b7f77655b05d17e43732ff158eb94abe66e6
vdkhai <khai@redweb.dk>
2013-12-06 16:56:31 +0700

    Site to site: migrate newsfeeds

---
+++ component/admin/includes/schemas/joomla25/newsfeeds.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * 
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+/**
+ * Upgrade class for Newsfeeds
+ *
+ * This class takes the newsfeeds from the existing site and inserts them into the new site.
+ *
+ * @since  0.4.5
+ */
+class RedMigratorNewsfeeds extends RedMigrator
+{
+	/**
+	 * Sets the data in the destination database.
+	 *
+	 * @return	void
+	 *
+	 * @since	3.0.
+	 * @throws	Exception
+	 */
+	public function dataHook($rows = null)
+	{
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row = (array) $row;
+
+			$row['id'] = null;
+
+			if ($row['created_by'] != '')
+			{
+				$row['created_by'] = RedMigratorHelper::lookupNewId('arrUsers', (int) $row['created_by']);
+			}
+
+			if ($row['modified_by'] != '')
+			{
+				$row['modified_by'] = RedMigratorHelper::lookupNewId('arrUsers', (int) $row['modified_by']);
+			}
+
+			if (version_compare(PHP_VERSION, '3.0', '>='))
+			{
+				unset($row['filename']);
+			}
+		}
+
+		return $rows;
+	}
+}
