38184cdfa9e84c8ed3d5f4121f6458be500faafe
vdkhai <khai@redweb.dk>
2013-12-20 15:46:24 +0700

    Integrate migration 1.5: migrate content frontpage

--- component/admin/includes/schemas/joomla15/content_frontpage.php
+++ component/admin/includes/schemas/joomla15/content_frontpage.php
@@ -15,4 +15,23 @@
  */
 class RedMigratorContentFrontpage extends RedMigrator
 {
+	/**
+	 * Sets the data in the destination database.
+	 *
+	 * @return	void
+	 */
+	public function dataHook($rows)
+	{
+		foreach ($rows as &$row)
+		{
+			$row = (array) $row;
+
+			if ($row['content_id'] != '')
+			{
+				$row['content_id'] = RedMigratorHelper::lookupNewId('arrContent', (int) $row['content_id']);
+			}
+		}
+
+		return $rows;
+	}
 }
