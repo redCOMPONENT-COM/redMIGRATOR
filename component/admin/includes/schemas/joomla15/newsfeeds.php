ff947d9a4de1050db4473e6e97c95c3d2f54cb1c
vdkhai <khai@redweb.dk>
2013-12-25 11:45:22 +0700

    Integrate migration 1.5: migrate newsfeeds

--- component/admin/includes/schemas/joomla15/newsfeeds.php
+++ component/admin/includes/schemas/joomla15/newsfeeds.php
@@ -20,6 +20,8 @@ class RedMigratorNewsfeeds extends RedMigrator
 	/**
 	 * Sets the data in the destination database.
 	 *
+	 * @param   array  $rows  Rows
+	 *
 	 * @return	void
 	 *
 	 * @since	3.0.
@@ -27,27 +29,27 @@ class RedMigratorNewsfeeds extends RedMigrator
 	 */
 	public function dataHook($rows = null)
 	{
-		// Getting the component parameter with global settings
-		$params = $this->getParams();
-
-		// Getting the categories id's
-		$categories = $this->getMapList('categories', 'com_newsfeeds');
-
 		// Do some custom post processing on the list.
 		foreach ($rows as &$row)
 		{
 			$row = (array) $row;
 
-			$row['access'] = 1;
-			$row['language'] = '*';
+			$row['id'] = null;
+
+			if ($row['created_by'] != '')
+			{
+				$row['created_by'] = RedMigratorHelper::lookupNewId('arrUsers', (int) $row['created_by']);
+			}
+
+			if ($row['modified_by'] != '')
+			{
+				$row['modified_by'] = RedMigratorHelper::lookupNewId('arrUsers', (int) $row['modified_by']);
+			}
 
 			if (version_compare(PHP_VERSION, '3.0', '>='))
 			{
 				unset($row['filename']);
 			}
-
-			$cid = $row['catid'];
-			$row['catid'] = &$categories[$cid]->new;
 		}
 
 		return $rows;
