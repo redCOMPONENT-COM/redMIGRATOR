e2ec34403377c3d41c04dc0f79df7f2665bf2014
vdkhai <khai@redweb.dk>
2014-01-23 11:29:02 +0700

    Fix media download path in redshop plugin

---
+++ plugins/3rd_party/plg_redmigrator_redshop/schemas/joomla15/redshop_media.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+
+class RedMigratorRedshopMedia extends RedMigrator
+{
+	public function dataHook($rows)
+	{
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row = (array) $row;
+
+			if ($row['media_type'] == "download")
+			{
+				$arrMediaName = explode("/", $row['media_name']);
+
+				$filename = $arrMediaName[count($arrMediaName) - 1];
+
+				$row['media_name'] = JPATH_ROOT . '/components/com_redshop/assets/download/product/' . $filename;
+			}
+		}
+
+		return $rows;
+	}
+}
+?>
---
+++ plugins/3rd_party/plg_redmigrator_redshop/schemas/joomla15/redshop_media_download.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+
+class RedMigratorRedshopMediaDownload extends RedMigrator
+{
+	public function dataHook($rows)
+	{
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row = (array) $row;
+
+			$arrName = explode("/", $row['name']);
+
+			$filename = $arrName[count($arrName) - 1];
+
+			$row['name'] = JPATH_ROOT . '/components/com_redshop/assets/download/product/' . $filename;
+		}
+
+		return $rows;
+	}
+}
+?>
